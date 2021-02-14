de8a47caa970438f6e7b16c9f592fdff9318cc55
Todd Stoneberg <todd.stoneberg@gmail.com>
2021-02-13 17:40:00 -0700

    building CRUD for Mountain and Hike in controllers and woking on views

--- app/controllers/hikes_controller.rb
+++ app/controllers/hikes_controller.rb
@@ -1,2 +1,35 @@
 class HikesController < ApplicationController
+
+    def new
+        @hike = Hike.new
+    end
+
+    def create
+        @hike = Hike.new(hike_params)
+
+        if @hike.save
+            redirect_to user_path(current_user)
+        else
+            render :new
+        end
+    end
+
+    def update
+        @hike = Hike.find_by_id(params[:id])
+
+        if @hike.update
+            redirect_to hikes_path
+        else
+            
+        end
+    end
+
+    def destroy
+
+    end
+
+    private
+    def hike_params
+        params.require(:hike).permit(:user_id, :mountain_id, :rating, :comment, :date_hiked, :percent_hiked, :hiked)
+    end
 end
--- app/controllers/mountains_controller.rb
+++ app/controllers/mountains_controller.rb
@@ -1,2 +1,39 @@
 class MountainsController < ApplicationController
+
+    def index
+        @mountains = Mountain.all
+    end
+
+    def show
+        @mountain = Mountain.find_by(id: params[:id])
+    end
+
+    def new
+        @mountain = Mountain.new
+    end
+
+    def create
+        @mountain = Mountain.new(mountain_params)
+
+        if @moutain.save
+            redirect_to mountain_path(@mountain)
+        else
+            render :new
+        end
+    end
+
+    def update 
+        @mountain = Mountain.find_by(id: params[:id])
+
+        if @moutain.save
+            redirect_to mountain_path(@mountain)
+        else
+            render :edit
+        end
+    end
+
+    private
+    def mountain_params
+        params.require[:mountain].permit[:base_elevation, :summit_elevation, :name, :city, :state, :direction_url, :hike_distance]
+    end
 end
