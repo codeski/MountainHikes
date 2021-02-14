079e005ad9167c075cdfd02fc1c2575f6da141fe
Todd Stoneberg <todd.stoneberg@gmail.com>
2021-02-13 23:52:20 -0700

    trying to build seed data

--- app/controllers/hikes_controller.rb
+++ app/controllers/hikes_controller.rb
@@ -6,7 +6,7 @@ class HikesController < ApplicationController
 
     def create
         @hike = Hike.new(hike_params)
-
+        @hike = @hike.mountain.build
         if @hike.save
             redirect_to user_path(current_user)
         else
@@ -16,16 +16,19 @@ class HikesController < ApplicationController
 
     def update
         @hike = Hike.find_by_id(params[:id])
-
+        
+        @mountain = @hike.mountain 
         if @hike.update
             redirect_to hikes_path
         else
-            
+            render :edit
         end
     end
 
     def destroy
-
+        @hike = Hike.find_by_id(params[:id])
+        @hike.destroy
+        redirect_to hikes_path
     end
 
     private
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Mountain.all.destroy
+User.all.destroy
+Hike.all.destroy
+
+sally = User.create(username: "Sally", admin: true, password: "sally", email: "sally@sally.com")
+fred = User.create(username: "Fred", password: "fred", email: "fred@fred.com")
+
+
+quandry = Mountain.create(name: "Quandry Peak", base_elevation: 11006, summit_elevation: 14270, city: "Breckenridge", state: "CO", directions_url: "https://goo.gl/maps/ACG5JVspvhkCykdz7", hike_distance: 7)
+baldy = Mountain.create(name: "Baldy", base_elevation: 10000, summit_elevation: 12890, city: "Breckenridge" , state: "CO", directions_url: "https://goo.gl/maps/Etsrxp2Dj17WXTkcA", hike_distance: 5)
+peak1 = Mountain.create(name: "Peak One", base_elevation: 8900, summit_elevation: 13400, city: "Frisco", state: "CO", directions_url: "https://goo.gl/maps/5qN9R3okHiou4VYu7", hike_distance: 6)
+buck = Mountain.create(name: "Buck", base_elevation: 0, summit_elevation: 300, city: "Burnsville", state: "MN", directions_url: "https://goo.gl/maps/HGCn2DadLZzACvgLA", hike_distance: 6)
+
+Hike.create(user_id: sally.id, mountain_id: quandry.id, rating: 4, comment: "My knees hurt", date_hiked: "2016-05-20", percent_hiked: 100, hiked: true)
+Hike.create(user_id: sally.id, mountain_id: baldy.id, rating: 5, comment: "So fun I'll do it again", date_hiked: "2014-04-20", percent_hiked: 100, hiked: true)
+Hike.create(user_id: sally.id, mountain_id: baldy.id, rating: 3, comment: "So busy, the secret is out", date_hiked: "2018-09-20", percent_hiked: 75, hiked: true)
+Hike.create(user_id: fred.id, mountain_id: quandry.id, rating: 2, comment: "Oxygen is way too thin", date_hiked: "2012-07-20", percent_hiked: 75, hiked: true)
+Hike.create(user_id: fred.id, mountain_id: buck.id, rating: 5, comment: "Took me 20 minutest to hike", date_hiked: "2006-08-20", percent_hiked: 75, hiked: true)
