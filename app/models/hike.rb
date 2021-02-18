6c8ed90b4b0f6f49f65dccbc4967de1a9aae2f26
Todd Stoneberg <todd.stoneberg@gmail.com>
2021-02-17 21:54:18 -0700

    adding validations and displaying errors

--- app/models/hike.rb
+++ app/models/hike.rb
@@ -2,8 +2,9 @@ class Hike < ApplicationRecord
     belongs_to :user
     belongs_to :mountain
 
-    def total_distance_elevation(mountain)
-        byebug
-        mountain.hike.total_distance = mountain.hike_distance * mountain.hike.hike_percentage
-    end
+    validates
+
+    # def total_distance_elevation(mountain)
+    #     mountain.hike.total_distance = mountain.hike_distance * mountain.hike.hike_percentage
+    # end
 end
--- app/models/mountain.rb
+++ app/models/mountain.rb
@@ -3,4 +3,6 @@ class Mountain < ApplicationRecord
     has_many :users, through: :links
     has_many :hikes
     has_many :users, through: :hikes
+
+    validates :name, :city, :state, :base_elevation, :summit_elevation, :hike_distance, :directions_url, presence: true
 end
