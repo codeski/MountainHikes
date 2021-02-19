be793f9d7908d50b200597f48ed016f3284cbeb6
Todd Stoneberg <todd.stoneberg@gmail.com>
2021-02-18 21:31:53 -0700

    added binding.pry, built class and instance methods for views, added custom validations for hike

--- app/models/hike.rb
+++ app/models/hike.rb
@@ -2,9 +2,25 @@ class Hike < ApplicationRecord
     belongs_to :user
     belongs_to :mountain
 
-    validates
+    validates :date_hiked, :percent_hiked, :rating, presence: true
+    validate :date_hiked_not_in_future
+    validates :percent_hiked, numericality: { only_integer: true, greater_than: 0, less_than_or_equal_to: 100 } 
+    validates :rating, numericality: { only_integer: true, greater_than: 0, less_than_or_equal_to: 5 }
+    validates :comment, length: { maximum: 300, too_long: "%{count} characters is the maximum allowed" } 
+    # validates :user_id, uniqueness: {message: "you've already reviewed this mountain"}
+
+    def date_hiked_not_in_future
+        if date_hiked.present? && date_hiked > Date.today
+            errors.add(:date_hiked, "can't be in the future")
+        end
+    end
+    
 
     # def total_distance_elevation(mountain)
     #     mountain.hike.total_distance = mountain.hike_distance * mountain.hike.hike_percentage
     # end
+
+    def self.order_by_date
+        self.order(date_hiked: :desc)
+    end
 end
--- app/models/mountain.rb
+++ app/models/mountain.rb
@@ -5,4 +5,18 @@ class Mountain < ApplicationRecord
     has_many :users, through: :hikes
 
     validates :name, :city, :state, :base_elevation, :summit_elevation, :hike_distance, :directions_url, presence: true
+
+    def display_rating
+        # binding.pry
+        display = "#{self.hikes.average(:rating)}".split(".")
+
+        if display.count == 0
+            "(no ratings yet)"
+        elsif display.count > 2
+            "#{display[0] + "." + display[1]}"
+        else
+            "#{display.join(".")}"
+        end
+    end
+
 end
